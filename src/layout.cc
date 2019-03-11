6e4decf9155ed8b33caafbf253d5291b331d784b
deurzen <m.vandeurzen@student.ru.nl>
2019-03-11 10:18:53 +0100

    adds tile layout

--- src/layout.cc
+++ src/layout.cc
@@ -16,7 +16,42 @@ layouthandler_t::layout_floating(const user_workspace_t& workspace) const
 void
 layouthandler_t::layout_tile(const user_workspace_t& workspace) const
 {
+    auto clients = workspace.get_all();
+    clients.erase(::std::remove_if(clients.begin(), clients.end(),
+        [](client_ptr_t client) { return client->floating; }), clients.end());
 
+    if (clients.empty())
+        return;
+
+    auto root_attrs = x_wrapper::get_attributes(x_wrapper::g_root);
+    dim_t screen_dim = {root_attrs.w() - m_ewmh.get_left_strut() - m_ewmh.get_right_strut(),
+        root_attrs.h() - m_ewmh.get_top_strut() - m_ewmh.get_bottom_strut()};
+
+    unsigned n_master = ::std::min(static_cast<unsigned>(clients.size()), workspace.get_n_master());
+
+    dim_t master_dim = {static_cast<int>(screen_dim.w * (n_master < clients.size()
+        ? workspace.get_m_factor() : 1)), n_master > 0 ? static_cast<int>(screen_dim.h / n_master) : 1};
+    dim_t stack_dim = {screen_dim.w - (n_master > 0 ? master_dim.w : 0),
+        screen_dim.h / static_cast<int>(n_master < clients.size() ? (clients.size() - n_master) : 1)};
+
+    pos_t master_pos = {m_ewmh.get_left_strut(), m_ewmh.get_top_strut()};
+    pos_t stack_pos  = {(n_master > 0 ? master_dim.w + 1: 0) + m_ewmh.get_left_strut(),
+        m_ewmh.get_top_strut()};
+
+    if (workspace.is_mirrored() && clients.size() > n_master && n_master != 0) {
+        ::std::swap(master_dim.w, stack_dim.w);
+        ::std::swap(master_pos.x, stack_pos.x);
+    }
+
+    for (size_t i = 0; i < n_master; ++i) {
+        clients[i]->resize(master_dim, true).move(master_pos, true);
+        master_pos.y += master_dim.h;
+    }
+
+    for (size_t i = n_master; i < clients.size(); ++i) {
+        clients[i]->resize(stack_dim, true).move(stack_pos, true);
+        stack_pos.y += stack_dim.h;
+    }
 }
 
 void
