031d136fbf3225d0db18c9e05f9f16bbaa604058
deurzen <m.vandeurzen@student.ru.nl>
2019-07-05 03:55:57 +0200

    changes default colors, border height

--- src/decoration.hh
+++ src/decoration.hh
@@ -6,7 +6,7 @@
 
 const ::std::string FONTNAME = "-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*";
 
-const int BORDER_HEIGHT   = 2;
+const int BORDER_HEIGHT   = 5;
 const int SHADED_HEIGHT   = 14;
 const int ICON_WIDTH      = 250;
 const int ICON_HEIGHT     = 16;
@@ -16,14 +16,14 @@ const int ICON_ROW_LENGTH = 50;
 const unsigned long SIDEBAR_FG_COLOR = 0xFFFFFF;
 const unsigned long SIDEBAR_BG_COLOR = 0x080808;
 
-const unsigned long SIDEBAR_LAYOUT_COLOR           = 0xFFFFFF;
-const unsigned long SIDEBAR_WORKSPACES_COLOR       = 0xBBBBBB;
-const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = 0xF0C674;
+const unsigned long SIDEBAR_LAYOUT_COLOR           = 0xFFFF00;
+const unsigned long SIDEBAR_WORKSPACES_COLOR       = 0xFFFFFF;
+const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = 0x00FF00;
 const unsigned long SIDEBAR_NCLIENTS_COLOR         = 0xFFFFFF;
 
 const unsigned long REG_COLOR = 0x404040;
-const unsigned long SEL_COLOR = 0xC0C0C0;
-const unsigned long URG_COLOR = 0xFF6347;
+const unsigned long SEL_COLOR = 0x0000FF;
+const unsigned long URG_COLOR = 0xFF0000;
 
 const unsigned long REG_SHADE_COLOR = 0x404040;
 const unsigned long SEL_SHADE_COLOR = 0x808080;
