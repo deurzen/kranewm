184aa267f293576f310edd10b22da0c23a00ba20
deurzen <m.vandeurzen@student.ru.nl>
2019-07-29 19:49:25 +0200

    changes default decoration and sidebar colors

--- src/decoration.hh
+++ src/decoration.hh
@@ -12,27 +12,27 @@ const int ICON_HEIGHT     = 16;
 const int ICON_BORDER     = 1;
 const int ICON_ROW_LENGTH = 50;
 
-const unsigned long SIDEBAR_FG_COLOR = 0xFFFFFF;
+const unsigned long SIDEBAR_FG_COLOR = 0xF5E2BD;
 const unsigned long SIDEBAR_BG_COLOR = 0x080808;
 
-const unsigned long SIDEBAR_LAYOUT_COLOR           = 0x61E8E1;
-const unsigned long SIDEBAR_CONTEXT_COLOR          = 0x7FB069;
-const unsigned long SIDEBAR_WORKSPACES_COLOR       = 0xFFFFFF;
-const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = 0xF4D35E;
-const unsigned long SIDEBAR_NSTICKY_COLOR          = 0xCB904D;
-const unsigned long SIDEBAR_NCLIENTS_COLOR         = 0xFFFFFF;
+const unsigned long REG_COLOR = 0x333333;
+const unsigned long SEL_COLOR = 0xDEB477;
+const unsigned long URG_COLOR = 0xDD614A;
+const unsigned long SELSTICKY_COLOR = 0xD3A26D;
+const unsigned long REGSTICKY_COLOR = 0x444444;
 
-const unsigned long REG_COLOR = 0x435058;
-const unsigned long SEL_COLOR = 0xF4D35E;
-const unsigned long URG_COLOR = 0xFF8360;
-const unsigned long SELSTICKY_COLOR = 0xCB904D;
-const unsigned long REGSTICKY_COLOR = 0x819595;
+const unsigned long SIDEBAR_LAYOUT_COLOR           = 0x7E99A8;
+const unsigned long SIDEBAR_CONTEXT_COLOR          = 0xB0BC98;
+const unsigned long SIDEBAR_WORKSPACES_COLOR       = SIDEBAR_FG_COLOR;
+const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = SEL_COLOR;
+const unsigned long SIDEBAR_NSTICKY_COLOR          = SELSTICKY_COLOR;
+const unsigned long SIDEBAR_NCLIENTS_COLOR         = SIDEBAR_WORKSPACES_COLOR;
 
-const unsigned long MRIND_BG_COLOR     = 0x000000;
-const unsigned long MRIND_BORDER_COLOR = 0xFFFFFF;
-const unsigned long FSIND_BG_COLOR     = 0x000000;
-const unsigned long FSIND_BORDER_COLOR = 0xFF0000;
-const unsigned long FLIND_BG_COLOR     = 0x000000;
-const unsigned long FLIND_BORDER_COLOR = 0xFFFFFF;
+const unsigned long MRIND_BG_COLOR     = REG_COLOR;
+const unsigned long MRIND_BORDER_COLOR = SIDEBAR_FG_COLOR;
+const unsigned long FSIND_BG_COLOR     = REG_COLOR;
+const unsigned long FSIND_BORDER_COLOR = URG_COLOR;
+const unsigned long FLIND_BG_COLOR     = REG_COLOR;
+const unsigned long FLIND_BORDER_COLOR = SIDEBAR_FG_COLOR;
 
 #endif//__KRANEWM__DECORATION__GUARD__
