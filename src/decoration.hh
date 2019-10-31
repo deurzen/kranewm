1e90c3e0b8a459a9530fa5b1f17d2b7d05db2cc6
deurzen <m.deurzen@tum.de>
2019-10-31 02:49:12 +0100

    updates colorscheme

--- src/decoration.hh
+++ src/decoration.hh
@@ -9,17 +9,17 @@ const ::std::string FONTNAME = "-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*";
 const int BORDER_HEIGHT = 3;
 
 const unsigned long SIDEBAR_FG_COLOR = 0xF8EBE7;
-const unsigned long SIDEBAR_BG_COLOR = 0x454745;
+const unsigned long SIDEBAR_BG_COLOR = 0x2D2D2D;
 
 const unsigned long REG_COLOR = 0x333333;
 const unsigned long SEL_COLOR = 0xED764D;
-const unsigned long URG_COLOR = 0x772953;
-const unsigned long DIS_COLOR = 0x2C001E;
-const unsigned long SELSTICKY_COLOR = 0x77216F;
+const unsigned long URG_COLOR = 0xF2777A;
+const unsigned long DIS_COLOR = 0x6699CC;
+const unsigned long SELSTICKY_COLOR = 0xCC99CC;
 const unsigned long REGSTICKY_COLOR = 0x444444;
 
-const unsigned long SIDEBAR_LAYOUT_COLOR           = 0x3DD6D0;
-const unsigned long SIDEBAR_CONTEXT_COLOR          = 0x71F79F;
+const unsigned long SIDEBAR_LAYOUT_COLOR           = 0xFFCC66;
+const unsigned long SIDEBAR_CONTEXT_COLOR          = 0x99CC99;
 const unsigned long SIDEBAR_WORKSPACES_COLOR       = SIDEBAR_FG_COLOR;
 const unsigned long SIDEBAR_ICONS_COLOR            = SIDEBAR_FG_COLOR;
 const unsigned long SIDEBAR_ICONNUMBERS_COLOR      = SIDEBAR_LAYOUT_COLOR;
