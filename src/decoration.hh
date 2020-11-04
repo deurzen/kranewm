75122b409a39f84c6022edb478b793413d2209b4
deurzen <m.deurzen@tum.de>
2020-11-05 00:08:20 +0100

    updates color scheme

--- src/decoration.hh
+++ src/decoration.hh
@@ -4,29 +4,28 @@
 #include <string>
 
 
-const ::std::string FONTNAME = "-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*";
 
 const int BORDER_HEIGHT = 3;
 
-const unsigned long SIDEBAR_FG_COLOR = 0xF8EBE7;
-const unsigned long SIDEBAR_BG_COLOR = 0x2D2D2D;
+const unsigned long SIDEBAR_FG_COLOR = 0xAEA79F;
+const unsigned long SIDEBAR_BG_COLOR = 0x1D1D1D;
 
-const unsigned long REG_COLOR = 0x333333;
-const unsigned long SEL_COLOR = 0xED764D;
-const unsigned long URG_COLOR = 0xF2777A;
-const unsigned long DIS_COLOR = 0x6699CC;
-const unsigned long SELSTICKY_COLOR = 0xCC99CC;
-const unsigned long REGSTICKY_COLOR = 0x444444;
+const unsigned long REG_COLOR = 0x191A2A;
+const unsigned long SEL_COLOR = 0xE95420;
+const unsigned long URG_COLOR = 0x31182F;
+const unsigned long DIS_COLOR = 0x636363;
+const unsigned long SELSTICKY_COLOR = 0x3802CA;
+const unsigned long REGSTICKY_COLOR = 0x2C001E;
 
-const unsigned long SIDEBAR_LAYOUT_COLOR           = 0xFFCC66;
-const unsigned long SIDEBAR_CONTEXT_COLOR          = 0x99CC99;
+const unsigned long SIDEBAR_LAYOUT_COLOR           = 0xF7F7F7;
+const unsigned long SIDEBAR_CONTEXT_COLOR          = 0xAEA79F;
 const unsigned long SIDEBAR_WORKSPACES_COLOR       = SIDEBAR_FG_COLOR;
 const unsigned long SIDEBAR_ICONS_COLOR            = SIDEBAR_FG_COLOR;
 const unsigned long SIDEBAR_ICONNUMBERS_COLOR      = SIDEBAR_LAYOUT_COLOR;
-const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = SEL_COLOR;
+const unsigned long SIDEBAR_ACTIVE_WORKSPACE_COLOR = 0xE95825;
 const unsigned long SIDEBAR_NSTICKY_COLOR          = SELSTICKY_COLOR;
 const unsigned long SIDEBAR_NCLIENTS_COLOR         = SIDEBAR_WORKSPACES_COLOR;
-const unsigned long ROOT_WORKSPACE_COLOR           = 0xFFFFFF;
+const unsigned long ROOT_WORKSPACE_COLOR           = 0x1D1D1D;
 
 // moveresize
 const unsigned long MRIND_BG_COLOR     = REG_COLOR;
@@ -47,7 +46,7 @@ const unsigned long BSIND_BORDER_COLOR = SELSTICKY_COLOR;
 const unsigned long ASIND_BG_COLOR     = REG_COLOR;
 const unsigned long ASIND_BORDER_COLOR = DIS_COLOR;
 // unmapped sidebar indicator
-const unsigned long USIND_BG_COLOR     = 0x000000;
-const unsigned long USIND_BORDER_COLOR = 0xFFFFFF;
+const unsigned long USIND_BG_COLOR     = 0x1D1D1D;
+const unsigned long USIND_BORDER_COLOR = 0xAEA79F;
 
 #endif//__KRANEWM__DECORATION__GUARD__
